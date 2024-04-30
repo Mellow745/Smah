b5ebc793d2f676da012683b5bdda81990e98f253
Melokuhle <melokuhlephilasande33@gmail.com>
2024-04-30 12:05:33 +0200

    AddingSpaceToSkillsClass

---
+++ source/repos/Simple_API_Assessment/Simple_API_Assessment/Models/Skill.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Simple_API_Assessment.Models
+{
+    public class Skill
+    {
+        [Key]
+        public int SkillId { get; set; }
+        public string? Name { get; set; }
+
+        [ForeignKey("ApplicantId")] 
+        public int ApplicantId { get; set; }
+
+        public Applicant? Applicant { get; set; } 
+
+    }
+}
